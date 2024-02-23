def7f6df153711e95b3f0c2f5e6afa668721cde8
Jonathan Pereira <jonathan14willian@gmail.com>
2024-02-23 20:28:23 -0300

    Atualizações de tradução e ajustes de layout

--- front/src/components/FlowList.vue
+++ front/src/components/FlowList.vue
@@ -1,51 +1,32 @@
 <template>
     <div>
         <div class="accordion" id="accordionExample">
-            <div class="accordion-item">
+            <div class="accordion-item mb-3">
                 <h2 class="accordion-header">
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
-                        aria-expanded="true" aria-controls="collapseOne">
-                        Accordion Item #1
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" :data-bs-target=" '#' + id "
+                        aria-expanded="true" :aria-controls="id">
+                        {{ arquivo }}
                     </button>
                 </h2>
-                <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                <div :id="id" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
                     <div class="accordion-body">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nemo temporibus magni illo, odio debitis nulla pariatur quis culpa optio dicta saepe harum ea voluptatem. Nemo perferendis ipsam reiciendis optio?
                     </div>
                 </div>
-            </div>
-            <div class="accordion-item">
-                <h2 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        Accordion Item #2
-                    </button>
-                </h2>
-                <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-                    <div class="accordion-body">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nemo temporibus magni illo, odio debitis nulla pariatur quis culpa optio dicta saepe harum ea voluptatem. Nemo perferendis ipsam reiciendis optio?
-                    </div>
-                </div>
-            </div>
-            <div class="accordion-item">
-                <h2 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                        Accordion Item #3
-                    </button>
-                </h2>
-                <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-                    <div class="accordion-body">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nemo temporibus magni illo, odio debitis nulla pariatur quis culpa optio dicta saepe harum ea voluptatem. Nemo perferendis ipsam reiciendis optio?
-                    </div>
-                </div>
-            </div>
+            </div>            
         </div>
     </div>
 </template>
 
 <script>
 export default {
+    name: 'FlowList',
+    props: {
+        msg: String,
+        arquivo: String,
+        loggers: Array,
+        id: String
+    }
 
 }
 </script>
--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -2,28 +2,31 @@
   <div>
     <header class="">
       <h1>Relação de loggers por fluxo</h1>
-  </header>
+    </header>
 
-  <main class="container mt-5 col-sm-10 col-md-10 col-lg-8">
+    <main class="container mt-5 col-sm-10 col-md-10 col-lg-8">
       <div class="row">
-          <aside class="col-sm-12 col-md-8">
-              <h2>Fluxos</h2>
-              <div class="mt-3">
-                <flow-list></flow-list>
-              </div>
-          </aside>
-          <section class="col-md-4">
-              <h3>Ambientes</h3>
-              <article class="mt-3">
-                  <client-list></client-list>
-              </article>
-          </section>
+        <aside class="col-sm-12 col-md-8">
+          <h2>Fluxos</h2>
+          <div class="mt-3">
+            <FlowList v-for="(flow, index) in flows" :key="index" :id="'collapse' + index" :arquivo="flow.arquivo"
+              :loggers="flow.loggers">
+            </FlowList>
+          </div>
+        </aside>
+        <section class="col-md-4">
+          <h3>Ambientes</h3>
+          <article class="mt-3">
+            <client-list></client-list>
+          </article>
+        </section>
       </div>
-  </main>  
-  <body>
-    
-      
-  </body>
+    </main>
+
+    <body>
+
+
+    </body>
   </div>
 </template>
 
@@ -34,8 +37,27 @@ export default {
   components: { ClientList, FlowList },
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+
+  },
+  data() {
+    return {
+      flows: [],
+      clients: []
+    }
+  },
+  methods: {
+    async getJsonData(jsonFile) {
+      const response = await fetch(`/database/${jsonFile}.json`);
+      const data = await response.json();
+      console.log(data);
+      return data;
+    }
+  },
+  async created() {
+    this.flows = await this.getJsonData('roge_loggers');
   }
+
 }
 </script>
 
@@ -44,14 +66,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
