45eac6eed8b8e668a0a2596e93701af11bbea97c
Jonathan Pereira <jonathan14willian@gmail.com>
2024-02-24 21:32:34 -0300

    Adiciona dependência do Vue Router e configuração inicial

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,17 +1,10 @@
 <template>
-  <FlowsPage />
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-import FlowsPage from './components/FlowsPage.vue'
-
-export default {
-  name: 'App',
-  components: {
-    FlowsPage
-  }
-}
-</script>
 
 <style>
 #app {
@@ -20,6 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ front/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <FlowPage />
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import FlowPage from '@/components/FlowsPage.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    FlowPage
+  }
+}
+</script>
